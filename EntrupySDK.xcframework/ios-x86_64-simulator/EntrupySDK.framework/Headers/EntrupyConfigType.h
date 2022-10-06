bd61e7765ef5c452ab9aeefbb912dc63411e3fc5
Felipe F Garcia <felipeflorencio@me.com>
2022-10-06 19:49:02 +0200

    Entrupy Beta SDK v1.0

---
+++ EntrupySDK.xcframework/ios-x86_64-simulator/EntrupySDK.framework/Headers/EntrupyConfigType.h
@@ -0,0 +1,16 @@
+//
+//  EntrupyConfigType.h
+//  EntrupySDK
+//
+//  Created by Dharini Raghavan on 9/8/22.
+//
+
+#ifndef EntrupyConfigType_h
+#define EntrupyConfigType_h
+
+typedef NS_ENUM(NSUInteger, EntrupyConfigType) {
+    ConfigTypeShort, //Short config is used to launch a shorter capture workflow. This is only to be used for dev testing
+    ConfigTypeFull   //The regular full length config
+};
+
+#endif /* EntrupyConfigType_h */
---
+++ EntrupySDK.xcframework/ios-x86_64-simulator/EntrupySDK.framework/Headers/EntrupySDK.h
@@ -0,0 +1,18 @@
+//
+//  EntrupySDK.h
+//  EntrupySDK
+//
+//  Created by Felipe Garcia on 12/04/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EntrupySDK.
+FOUNDATION_EXPORT double EntrupySDKVersionNumber;
+
+//! Project version string for EntrupySDK.
+FOUNDATION_EXPORT const unsigned char EntrupySDKVersionString[];
+
+#import <EntrupySDK/EntrupyApp.h>
+#import <EntrupySDK/EntrupyAppProtocols.h>
+#import <EntrupySDK/EntrupyErrorCode.h>
